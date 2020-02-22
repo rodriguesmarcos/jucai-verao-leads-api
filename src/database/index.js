9b27491f3f13c7ea0d4001489c51e100cbfe304f
Marcos Rodrigues <eu@marcosrodrigues.me>
2020-02-22 00:12:39 -0300

    refactor: add mongo auth setting

--- src/database/index.js
+++ src/database/index.js
@@ -7,6 +7,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      authSource: 'admin',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
